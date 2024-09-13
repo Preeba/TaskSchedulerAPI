8469b11ed3d38d0e9352ed7c8742172546cfe2a0
preebs <preeba@gmail.com>
2024-09-13 14:00:40 -0700

    Testing & Enabling GraphiQL on port 8080

--- TaskSchedulerAPI/src/main/java/org/example/taskscheduler/input/TaskInput.java
+++ TaskSchedulerAPI/src/main/java/org/example/taskscheduler/input/TaskInput.java
@@ -1,9 +1,11 @@
 package org.example.taskscheduler.input;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.example.taskscheduler.model.RecurrenceSchedule;
 
 @Data
+@AllArgsConstructor
 public class TaskInput {
     private String name;
     private String description;
